58eeade93a8ca7b70f8297652c642b826737d256
Yogina Khandelwal <yogina.khandelwal@gmail.com>
2024-10-05 17:34:40 -0400

    removed hyperlinks for now

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,54 +1,112 @@
 import React from "react";
-import mock01 from '../assets/images/Reflectify.png';
-import mock02 from '../assets/images/Recipe Spark.png';
-import mock03 from '../assets/images/BrainFlix.png';
-import mock04 from '../assets/images/Instock.png';
-import mock05 from '../assets/images/Bandsite.png';
-import mock06 from '../assets/images/coffeeshop.png';
-import '../assets/styles/Project.scss';
+import mock01 from "../assets/images/Reflectify.png";
+import mock02 from "../assets/images/Recipe Spark.png";
+import mock03 from "../assets/images/BrainFlix.png";
+import mock04 from "../assets/images/Instock.png";
+import mock05 from "../assets/images/Bandsite.png";
+import mock06 from "../assets/images/coffeeshop.png";
+import "../assets/styles/Project.scss";
 
 function Project() {
-    return(
+  return (
     <div className="projects-container" id="projects">
-        <h1>Personal Projects</h1>
-        <div className="projects-grid">
-            <div className="project">
-                <a href="#" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="#" target="_blank" rel="noreferrer"><h2>Reflectify</h2></a>
-                <p>Developed mental wellness toolkit by integrating features like data visualization, personalized AI powered insights and self reflection tools using React, Bootstrap, Chart JS, Node JS, Express, MySQL and OpenAI API.</p>
-            </div>
-            <div className="project">
-                <a href="" target="_blank" rel="noreferrer"><img src={mock02} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="" target="_blank" rel="noreferrer"><h2>Recipe Spark</h2></a>
-                <p>A recipe website developed by using React, CSS, Javascript and Meal DB API.</p>
-            </div>  
-            <div className="project">
-                <a href="" target="_blank" rel="noreferrer"><img src={mock03} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="" target="_blank" rel="noreferrer"><h2>BrainFlix</h2></a>
-                <p>BrainFlix is a fully responsive YouTube clone; showcasing dynamically filtered Next Video suggestions, page routing done with react-router and the ability to upload a mock-video to a self-built Node.js backend which served as the REST API for the application.</p>
-
-            </div> 
-            <div className="project">
-                <a href="" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="" target="_blank" rel="noreferrer"><h2>Instock</h2></a>
-                <p>InStock is a fully responsive inventory management application that allows users to keep track of inventories spread across multiple warehouses. Users can add, edit, delete both warehouses and inventory items. The app itself was built with React and SASS on the frontend and Node, Express on the backend.</p>
-
-            </div>
-            <div className="project">
-                <a href="" target="_blank" rel="noreferrer"><img src={mock05} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="" target="_blank" rel="noreferrer"><h2>Bandsite</h2></a>
-                <p>Bandsite is a fully responsive front-end application for a fictional band by the name of 'The Bees Knees'. This project was built using HTML and vanilla JavaScript, along with Sass for all styling and visual responsiveness. Complete with dynamic data being pulled from an Express API hosted on Heroku with the added ability to be able to post, like and delete a comment.</p>
-
-            </div>
-            <div className="project">
-                <a href="" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="" target="_blank" rel="noreferrer"><h2>Coffee Shop</h2></a>
-                <p>Coffeeshop aka. Nitro Coffee is a static, multi-page coffee shop website built using HTML and CSS, displaying various menu items, promotions and contact information for the business.</p>
-
-            </div>
+      <h1>Personal Projects</h1>
+      <div className="projects-grid">
+        <div className="project">
+          {/* <a href="#" target="_blank" rel="noreferrer"> */}
+            <img src={mock01} className="zoom" alt="thumbnail" width="100%" />
+          {/* </a> */}
+          {/* <a href="#" target="_blank" rel="noreferrer"> */}
+            <h2>Reflectify</h2>
+          {/* </a> */}
+          <p>
+            Developed mental wellness toolkit by integrating features like data
+            visualization, personalized AI powered insights and self reflection
+            tools using React, Bootstrap, Chart JS, Node JS, Express, MySQL and
+            OpenAI API.
+          </p>
         </div>
+        <div className="project">
+          {/* <a href="" target="_blank" rel="noreferrer"> */}
+            <img src={mock02} className="zoom" alt="thumbnail" width="100%" />
+          {/* </a> */}
+          {/* <a href="" target="_blank" rel="noreferrer"> */}
+            <h2>Recipe Spark</h2>
+        {/* </a> */}
+          <p>
+            A recipe website developed by using React, CSS, Javascript and Meal
+            DB API.
+          </p>
+        </div>
+        <div className="project">
+          {/* <a href="" target="_blank" rel="noreferrer"> */}
+            <img src={mock03} className="zoom" alt="thumbnail" width="100%" />
+          {/* </a> */}
+          {/* <a href="" target="_blank" rel="noreferrer"> */}
+            <h2>BrainFlix</h2>
+         {/* </a> */}
+          <p>
+            BrainFlix is a fully responsive YouTube clone; showcasing
+            dynamically filtered Next Video suggestions, page routing done with
+            react-router and the ability to upload a mock-video to a self-built
+            Node.js backend which served as the REST API for the application.
+          </p>
+        </div>
+        <div className="project">
+          {/* <a href="" target="_blank" rel="noreferrer"> */}
+            <img src={mock04} className="zoom" alt="thumbnail" width="100%" />
+          {/* </a> */}
+          {/* <a href="" target="_blank" rel="noreferrer"> */}
+            <h2>Instock</h2>
+         {/* </a> */}
+          <p>
+            InStock is a fully responsive inventory management application that
+            allows users to keep track of inventories spread across multiple
+            warehouses. Users can add, edit, delete both warehouses and
+            inventory items. The app itself was built with React and SASS on the
+            frontend and Node, Express on the backend.
+          </p>
+        </div>
+        <div className="project">
+          {/* <a
+            href="https://drive.google.com/drive/folders/1waxbeQmIlD7Atxq3iUjWN0CAwGQnGq6D"
+            target="_blank"
+            rel="noreferrer"
+          > */}
+            <img src={mock05} className="zoom" alt="thumbnail" width="100%" />
+          {/* </a> */}
+          {/* <a
+            href="https://drive.google.com/drive/folders/1waxbeQmIlD7Atxq3iUjWN0CAwGQnGq6D"
+            target="_blank"
+            rel="noreferrer"
+          > */}
+            <h2>Bandsite</h2>
+          {/* </a> */}
+          <p>
+            Bandsite is a fully responsive front-end application for a fictional
+            band by the name of 'The Bees Knees'. This project was built using
+            HTML and vanilla JavaScript, along with Sass for all styling and
+            visual responsiveness. Complete with dynamic data being pulled from
+            an Express API hosted on Heroku with the added ability to be able to
+            post, like and delete a comment.
+          </p>
+        </div>
+        <div className="project">
+          {/* <a href="" target="_blank" rel="noreferrer"> */}
+            <img src={mock06} className="zoom" alt="thumbnail" width="100%" />
+          {/* </a> */}
+          {/* <a href="" target="_blank" rel="noreferrer"> */}
+            <h2>Coffee Shop</h2>
+         {/* </a> */}
+          <p>
+            Coffeeshop aka. Nitro Coffee is a static, multi-page coffee shop
+            website built using HTML and CSS, displaying various menu items,
+            promotions and contact information for the business.
+          </p>
+        </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default Project;
